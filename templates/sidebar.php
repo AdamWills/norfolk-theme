5ab2ee7308d115cbe6b5a51f343482a0d41f9811
Adam Wills <heavymetaladam@gmail.com>
2016-06-26 14:37:42 -0400

    Show siblings if any exist

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -15,7 +15,7 @@
   $ancestors = get_post_ancestors($post->ID);
   $parent_title = get_the_title( $post->post_parent );
 
-  if (count($ancestors) > 1) :
+  if (count($ancestors) > 0) :
     $siblingPages = wp_list_pages( 'sort_column=menu_order&title_li=&depth=1&child_of=' . $post->post_parent . '&exclude='. $post->ID .'&sort_column=title&echo=0' );
     if ($siblingPages) : ?>
       <h2><?= __('More About','sage'); ?> <span class="page-title"><?= $parent_title ?></span></h2>
