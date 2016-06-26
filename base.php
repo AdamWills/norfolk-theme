d3083c35decbdce22030a5425db9fa784f2502e5
Adam Wills <heavymetaladam@gmail.com>
2016-06-26 13:23:30 -0400

    Added "skip to content" link

--- base.php
+++ base.php
@@ -18,7 +18,7 @@ use Roots\Sage\Wrapper;
       get_template_part('templates/header');
     ?>
     <div class="wrap container" role="document">
-      <div class="content row">
+      <div id="content" class="content row" tabindex="-1">
         <main class="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,9 @@
 <header class="banner">
+    <a href="#content" class="a11y-skip-content sr-only sr-only-focusable"><?= __('Skip to main content', 'sage'); ?></a>
     <div class="logo">
       <a  href="<?= esc_url(home_url('/')); ?>"><?php get_template_part( 'dist/images/logo.svg' ); ?></a>
     </div>
-    <button type="button" class="btn-mobile-nav-toggle btn-link">Menu</button>
+    <button type="button" class="btn-mobile-nav-toggle btn-link"><?= __('Menu', 'sage'); ?></button>
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'banner__nav', 'container' => '', 'walker' => new Norfolk_Nav_Walker()]);
