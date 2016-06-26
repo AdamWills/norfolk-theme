044799556a400dc825499f16a3044f470cf647cd
Adam Wills <heavymetaladam@gmail.com>
2016-06-26 16:37:41 -0400

    Using Open Sans

--- templates/footer.php
+++ templates/footer.php
@@ -5,3 +5,16 @@
       endif;
       ?>
 </footer>
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Open+Sans:400,600,700,400italic:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+  </script>
