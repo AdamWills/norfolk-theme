1d1483c908601d0b9c76ccf6904a72eed4513d32
Adam Wills <heavymetaladam@gmail.com>
2016-07-06 19:16:52 -0400

    Search results!

--- search.php
+++ search.php
@@ -1,14 +1,5 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'search'); ?>
-<?php endwhile; ?>
+<gcse:searchresults-only linktarget="_parent"></gcse:searchresults-only>
 
 <?php the_posts_navigation(); ?>
